16134dd473171a2088c393b0496bfeb349dd2a28
KK-it-debug <khushikheti600@gmail.com>
2024-07-26 23:58:50 -0600

    Add files via upload

---
+++ Lab03_Game/Lab03_Game/Animation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab03_Game
+{
+    public delegate void ThreadDelay();
+    public partial class Animation : Form
+    {
+        public ThreadDelay AnimationDelay = null;
+        public Animation()
+        {
+            InitializeComponent();
+        }
+        
+        public int FallDelay
+        {
+            get
+            {
+                return UI_TrackBar.Value;
+            }
+        }
+
+        private void Animation_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                AnimationDelay();
+                e.Cancel = true;
+            }
+
+            Hide();
+        }
+    }
+}
---
+++ Lab03_Game/Lab03_Game/SelectDifficulty.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab03_Game
+{
+    public partial class SelectDifficulty : Form
+    {
+        public int Choice
+        {
+            get
+            {
+                return DiffLevel;
+            }
+        }
+        public SelectDifficulty()
+        {
+            InitializeComponent();
+        }
+        int DiffLevel;
+        private void UI_Level_Radio(object sender, EventArgs e)
+        {
+            if(UI_Easy_Radio.Checked)
+            {
+                DiffLevel = 3;
+            }
+            if(UI_Medium_Radio.Checked)
+            {
+                DiffLevel = 4;
+            }
+            if(UI_Difficulty_Radio.Checked)
+            {
+                DiffLevel = 5;
+            }
+        }
+
+        private void UI_OK_btn_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
+
+        private void UI_Cancel_btn_Click(object sender, EventArgs e)
+        {
+            DialogResult=DialogResult.Cancel;
+        }
+    }
+}
